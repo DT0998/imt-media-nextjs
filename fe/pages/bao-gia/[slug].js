4aacbcca280da66b91134cdf5c684e59136ecca9
toan-2406 <57821796+toan-2406@users.noreply.github.com>
2022-03-29 13:09:41 +0700

    update

--- fe/pages/bao-gia/[slug].js
+++ fe/pages/bao-gia/[slug].js
@@ -1,5 +1,8 @@
 import React from "react";
+import { useRouter } from "next/router";
 export default function index() {
+  const router = useRouter();
+  router.query.slug = "bao-gia-dich-vu-quay-phim";
   return (
     <div>
       <h1>hello</h1>
--- fe/pages/dich-vu/index.js
+++ fe/pages/dich-vu/index.js
@@ -1,5 +1,46 @@
 import React from "react";
+import {
+  Helmet,
+  HeroFlat,
+  Section,
+  SectionBody,
+  SectionTitle,
+} from "../../components";
+import Image from "next/image";
+//data
+import ProjectData from "../../data/fake-data/project";
+//style
+import styles from "./Service.module.scss";
 
 export default function Service() {
-  return <div>Service</div>;
+  const projects = ProjectData.getProjects();
+  return (
+    <Helmet title="Dịch vụ">
+      <HeroFlat page="Dịch vụ" />
+      <Section>
+        <SectionTitle>
+          <h1>Các dịch vụ của chúng tôi</h1>
+        </SectionTitle>
+        <SectionBody>
+          <div className={styles.serviceCard}>
+            {projects.map((item, index) => (
+              <div className={styles.serviceCard__Item} key={index}>
+                <div className={styles.serviceCard__Item__Image}>
+                  <Image
+                    src={item.thumbnail.src}
+                    alt="Lorem ipsum dolor sit"
+                    layout="fill"
+                  />
+                </div>
+                <div className={styles.serviceCard__Item__Content}>
+                  <h3>Lorem ipsum dolor sit</h3>
+                  <p>lorem ipsum dt Lorem ipsum dolor sit</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </SectionBody>
+      </Section>
+    </Helmet>
+  );
 }

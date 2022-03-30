b1ecd2e2749b7ea945efd5a72e06b9806ba18457
DT0998 <duongkytrieuvn@gmail.com>
2022-03-30 21:32:03 +0700

    update

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-    url: env('https://strapimovie.herokuapp.com/'),
+    url: env('https://strapimovie.herokuapp.com/admin/auth/register-admin'),
     proxy: true,
     app: {
       keys: env.array("APP_KEYS"),
